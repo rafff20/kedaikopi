62411f9932040876cfbb3dbbd34085598a9db973
rafff20 <rizqiafauzan@gmail.com>
2024-03-10 13:48:38 +0700

    penambahan search

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
-// Toogle class active
+// Toogle class active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 // ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
+};
+
+//toogle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+document.querySelector("#search-button").onclick = () => {
+  searchForm.classList.toggle("active");
 };
 
 // Klik diluar sidebard untuk menhilangkan nav
